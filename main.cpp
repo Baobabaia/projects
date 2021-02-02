7cad893800436263d2b65e27f62e52e656571097
Baobabaia <78372663+Baobabaia@users.noreply.github.com>
2021-02-02 17:38:03 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+    
+int c1,c2,k1,k2,oc1,oc2,d;
+
+cout<<"2"<<endl;
+  cin>>k1;
+cout<<"3"<<endl;
+  cin>>k2;
+cout<<"2"<<endl;
+   cin>>c2;
+cout<<"5"<<endl;
+   cin>>c1;
+
+  oc1 = k1*c1;
+  oc2 = k2*c2;
+  d = oc1-oc2;
+
+cout<<"4 = "<<d<<endl;
+cout<<"6 "<<oc2<<endl;
+cout<<"10 "<<oc1<<endl;
+
+  getch();
+  return 0;
+}
